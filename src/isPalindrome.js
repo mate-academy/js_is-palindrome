0e32ad90df5fbd38cb3d63cd7b7ebceba7379153
unknown <dumavb777@gmail.com>
2019-04-15 18:14:07 +0300

    'Solution'

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -19,7 +19,28 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  // write code here
+  if (str === '') {
+    return true;
+  }// I don't understand how to do this more simply
+  const resultOfMatch = str.match(/\w/g);
+  const reverseResultOfMatch = str.match(/\w/g).reverse();
+  let firstHalfOfString;
+  let secondHalfOfString;
+  const lengthDivideByTwo = resultOfMatch.length % 2;
+  const cutEvenArray = (array) => array.slice(0, resultOfMatch.length / 2)
+    .map(letter => letter.toLowerCase()).join('');
+  const cutOddArray = (array) => array.slice(0, (resultOfMatch.length - 1) / 2)
+    .map(letter => letter.toLowerCase()).join('');
+
+  if (lengthDivideByTwo === 0) {
+    firstHalfOfString = cutEvenArray(resultOfMatch);
+    secondHalfOfString = cutEvenArray(reverseResultOfMatch);
+  } else if (lengthDivideByTwo === 1) {
+    firstHalfOfString = cutOddArray(resultOfMatch);
+    secondHalfOfString = cutOddArray(reverseResultOfMatch);
+  }
+
+  return firstHalfOfString === secondHalfOfString;
 }
 
 module.exports = isPalindrome;
