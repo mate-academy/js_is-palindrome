1551bbf78e250696e5c25e1bce034389ab4371ba
Leon <FeelineKing@gmail.com>
2019-10-11 20:02:34 +0300

    Solution

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -19,7 +19,10 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  // write code here
+  const replacedString = str.replace(/\W/g, '').toLowerCase();
+  const reversedString = replacedString.split('').reverse().join('');
+
+  return replacedString === reversedString;
 }
 
 module.exports = isPalindrome;
