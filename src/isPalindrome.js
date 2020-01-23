4b58dcf150e76bb57fb9f7751838c1801ab0f106
ded-git <sakarailko@gmail.com>
2020-01-23 15:59:29 +0200

    regex

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -20,6 +20,22 @@
  */
 function isPalindrome(str) {
   // write code here
+  const regStr = /\w/g;
+
+  if (str === '') {
+    return true;
+  }
+
+  const lowerStr = (str.toLowerCase()).match(regStr);
+  const reverseLowerStr = ((str.toLowerCase()).match(regStr)).reverse();
+
+  for (let i = 0; i < lowerStr.length; i++) {
+    if (lowerStr[i] !== reverseLowerStr[i]) {
+      return false;
+    } else if (i === lowerStr.length - 1) {
+      return true;
+    }
+  }
 }
 
 module.exports = isPalindrome;
