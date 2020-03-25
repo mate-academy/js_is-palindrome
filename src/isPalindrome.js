0f1e37ef6bbef10b329c0244db71c3c33705f3fb
Rustam Khananov <antmine2017@gmail.com>
2020-03-25 19:38:54 +0200

    solution

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -19,7 +19,22 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  // write code here
+  const arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (", '-:".includes(arr[i])) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    if (arr[i].toUpperCase() !== arr[arr.length - 1 - i].toUpperCase()) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isPalindrome;
