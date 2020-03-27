357c73f2e344bb399ccf077acac19bffebdd04c8
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-03-27 14:10:56 +0200

    updated variable

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -24,19 +24,19 @@ function isPalindrome(str) {
   }
 
   const strWithoutSym = str.toLowerCase().match(/[a-z]/g);
-  let strPart1;
-  let strPart2;
+  let firstHalf;
+  let secondHalf;
   const len = strWithoutSym.length;
 
   if (len % 2 === 0) {
-    strPart1 = strWithoutSym.slice(0, len / 2).join('');
-    strPart2 = strWithoutSym.slice(-(len / 2)).reverse().join('');
+    firstHalf = strWithoutSym.slice(0, len / 2).join('');
+    secondHalf = strWithoutSym.slice(-(len / 2)).reverse().join('');
   } else {
-    strPart1 = strWithoutSym.slice(0, (len / 2) - 1).join('');
-    strPart2 = strWithoutSym.slice(-(len / 2) + 1).reverse().join('');
+    firstHalf = strWithoutSym.slice(0, (len / 2) - 1).join('');
+    secondHalf = strWithoutSym.slice(-(len / 2) + 1).reverse().join('');
   }
 
-  return strPart1 === strPart2;
+  return firstHalf === secondHalf;
 }
 
 module.exports = isPalindrome;
