8dbed8420acf042997ae3e2df784372ce287de35
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-05-25 12:55:24 +0300

    solution

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -19,7 +19,7 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  const cache = str.replace(/[-/.',:!?;' ']*/g, '').toLowerCase();
+  const cache = str.replace(/[\W ' ']/g, '').toLowerCase();
   const reverse = cache.split('').reverse().join('');
 
   if (cache === reverse) {
