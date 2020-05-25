b89c40da18dee633796924483f2e9d75822120f0
German <germandolia@gmail.com>
2020-05-25 21:57:43 +0300

    solution

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -19,7 +19,11 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  // write code here
+  const string = str
+    .replace(/[^\w]/gi, '')
+    .toLowerCase();
+
+  return string === string.split('').reverse().join('');
 }
 
 module.exports = isPalindrome;
