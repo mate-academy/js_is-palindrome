cd6869ecea73cd65b52bf3b7459a7d378fb1335c
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-05-26 11:40:15 +0300

    solution

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -19,7 +19,21 @@
  * @return {boolean}
  */
 function isPalindrome(str) {
-  // write code here
+  if (!str) {
+    return true;
+  }
+
+  const input = str.match(/\w/g).join('').toLowerCase();
+
+  let lettersMatches = 0;
+
+  for (let i = 0; i < input.length / 2; i++) {
+    if (input[i] === input[input.length - (i + 1)]) {
+      lettersMatches++;
+    }
+  }
+
+  return lettersMatches >= input.length / 2;
 }
 
 module.exports = isPalindrome;
