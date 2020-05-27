e687abd5288916070d8401155167ee91801263da
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-05-27 17:26:29 +0300

    fixed comments

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -23,16 +23,14 @@ function isPalindrome(str) {
     return true;
   }
 
-  const input = str.match(/\w/g).join('').toLowerCase();
-  let lettersMatches = 0;
+  const filteredInput = str.replace(/\W/g, '').toLowerCase();
+  const reversedInput = filteredInput.split('').reverse().join('');
 
-  for (let i = 0; i < input.length / 2; i++) {
-    if (input[i] === input[input.length - (i + 1)]) {
-      lettersMatches++;
-    }
+  if (filteredInput === reversedInput) {
+    return true;
+  } else {
+    return false;
   }
-
-  return lettersMatches >= input.length / 2;
 }
 
 module.exports = isPalindrome;
